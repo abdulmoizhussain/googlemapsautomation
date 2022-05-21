fe06ebfb20b8e8036dc37d7ea6ad653ec0d5c633
Abdul Moiz <moizhus123@gmail.com>
2022-05-22 01:43:18 +0500

    why do the thing manually when you can spend hours automating it?

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 //sources: 
 // https://www.youtube.com/watch?v=BQ-9e13kJ58
 // https://www.npmjs.com/package/chromedriver
+// https://nodejs.dev/learn/how-to-read-environment-variables-from-nodejs
 
+require('dotenv').config();
 require('chromedriver');
 const { Builder, By, Key } = require("selenium-webdriver");
 
+const startingPoint = process.env["starting_point"];
+const destination = process.env["destination"];
+
 async function index() {
-
-  const chrome = await new Builder().forBrowser("chrome").build();
+  const driver = await new Builder().forBrowser("chrome").build();
 
   await waitFor(1500);
-  await chrome.get("https://www.google.com/maps");
-
-  const input = await chrome.findElement(By.css("input#searchboxinput"));
-  await waitFor(1500);
-
+  // await chrome.get("https://www.google.com/maps");
+  await driver.get("https://www.google.com/maps/dir///@44.9763834,-93.2797639,11.97z/data=!4m2!4m1!3e0?hl=en");
   // direct directions URL:
   // "https://www.google.com/maps/dir///@44.9763834,-93.2797639,11.97z/data=!4m2!4m1!3e0?hl=en"
-  
-  input.sendKeys("starting_point");
-  // input.sendKeys("destination");
+
+  // const input = await chrome.findElement(By.css("input#searchboxinput"));
+  const startingPointWebElement = await driver.findElement(By.css('input[aria-label="Choose starting point, or click on the map..."]'));
+  const destinationWebElement = await driver.findElement(By.css('input[aria-label="Choose destination..."]'));
+  await waitFor(1500);
+
+  await startingPointWebElement.sendKeys(startingPoint);
+  await waitFor(1500);
+  await destinationWebElement.sendKeys(destination);
 
   await waitFor(1500);
-  await input.sendKeys(Key.RETURN);
+
+  // EITHER THIS:
+  await destinationWebElement.sendKeys(Key.ENTER);
+  // OR THIS:
+  // const searchButtonWebElement = await driver.findElement(By.css('div#directions-searchbox-1 button[aria-label="Search"]'));
+  // await searchButtonWebElement.click();
+
+  const optionsButtonWebElement = await driver.findElement(By.css('button[jstcache="47"]'));
+  await optionsButtonWebElement.click();
+
+  await waitFor(1000);
+  const highwayOptionWebElement = await driver.findElement(By.css("label[for='pane.directions-options-avoid-highways']"));
+  await highwayOptionWebElement.click();
+  await waitFor(500);
+  const tollsOptionWebElement = await driver.findElement(By.css("label[for='pane.directions-options-avoid-tolls']"));
+  await tollsOptionWebElement.click();
+
+  // await waitFor(1500);
 }
 
 index();
